d231a0b23c792feae96f0988d6c98de3ba9111ba
FloezeTv <mathematikschlumpf@gmail.com>
2021-12-10 01:58:07 +0100

    Made ResourceLoader more robust (non-existent and empty files)

--- common/io/src/main/java/tv/floeze/bottleengine/common/io/ResourceLoader.java
+++ common/io/src/main/java/tv/floeze/bottleengine/common/io/ResourceLoader.java
@@ -1,5 +1,7 @@
 package tv.floeze.bottleengine.common.io;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public final class ResourceLoader {
@@ -8,8 +10,12 @@ public final class ResourceLoader {
 	}
 
 	public static String loadTextFromResources(String path) {
-		try (Scanner s = new Scanner(ResourceLoader.class.getClassLoader().getResourceAsStream(path))
-				.useDelimiter("\\A")) {
+		InputStream stream = ResourceLoader.class.getClassLoader().getResourceAsStream(path);
+		if (stream == null)
+			throw new RuntimeException(new FileNotFoundException("Could not find " + path + " on classpath"));
+		try (Scanner s = new Scanner(stream).useDelimiter("\\A")) {
+			if (!s.hasNext())
+				return "";
 			return s.next();
 		}
 	}
